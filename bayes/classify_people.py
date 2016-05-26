69a5d15ce6f0c27fb880cb1581fa3e6c684d130e
a-leut <turbomonkeyninja@gmail.com>
2016-05-26 02:20:45 -0600

    files

--- bayes/classify_people.py
+++ bayes/classify_people.py
@@ -1,16 +1,24 @@
 import numpy as np
+import pandas as pd
+from tqdm import tqdm
 from sklearn import mixture
 
+COMPONENTS = 25
+SAMPLES = 10
+
 def get_clusters(array):
-    dpgmm = mixture.DPGM(n_components=25)
+    dpgmm = mixture.DPGMM(n_components=COMPONENTS)
     dpgmm.fit(array)
     return dpgmm.predict(array)
 
 def main():
     with open('clean_data.csv', 'rb') as f:
-        x = np.loadtext(f, delimiter=',', skiprows=1)
-        clusters = get_clusters()
-        print(clusters)
+        x = np.loadtxt(f, delimiter=',', skiprows=1)
+    res = pd.DataFrame()
+    for i in tqdm(range(SAMPLES)):
+        clusters = get_clusters(x).astype('int')
+        res['t%d' % i] = clusters
+    res.to_csv('cluster_data_%s.csv' % COMPONENTS, index=False)
 
 if __name__ == '__main__':
     main()
